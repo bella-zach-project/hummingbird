cfa17f622292fb64f3268f2733c4a25eb2c766bf
Wen-King Su <wksrpi335@gmail.com>
2020-06-13 17:44:33 -0700

    add some operations to cli

--- bella/cli.py
+++ bella/cli.py
@@ -1,4 +1,4 @@
-stk = [ 'this', 'is', 123 ]
+stk = [ 'this', 'is', 123, 456 ]
 def list():
 	for a in stk:
 		print(a)
@@ -6,4 +6,11 @@ def list():
 while True:
 	list()
 	v = input('> ')
-	stk.append(v)
+	if v == 'd':
+	   stk = stk[:-1]
+	elif v == '+':
+	   s = int(stk[-1]) + int(stk[-2])
+	   stk = stk[:-2]
+	   stk.append(s)
+	else:
+	   stk.append(v)
