d617372e2ab0c7669869003579d0123cda6906b0
isabellaesu <isabellaesu@gmail.com>
2020-06-28 16:28:57 -0700

    epi

--- bella/square.py
+++ bella/square.py
@@ -4,7 +4,37 @@ from os.path import dirname, join, abspath
 sys.path.append(abspath(join(dirname(__file__), '..')))
 from zach import canvas_project as zvc
 
+def circle(ox, oy, s):
+	global cv
+	cv.new_graph()
+	for i in range(17):
+		a1 = i * ((2 *(math.pi)) / 16)
+		x1 = s * (math.cos(a1)) + ox
+		y1 = s * (math.sin(a1)) + oy
+		cv.vert(x1,y1)	
 
+
+class epi():
+	def __init__(self, s, sa, rate, p = None):
+		self.s = s
+		self.sa = sa
+		self.p = p
+		self.rate = rate
+	def draw(self, ox, oy, t):
+		global cv
+		# circle(ox, oy, self.s)
+		a1 = (t * self.rate) + self.sa
+		x1 = self.s * (math.cos(a1)) + ox
+		y1 = self.s * (math.sin(a1)) + oy
+		if self.p:
+			self.p.draw(x1, y1, t)
+		else:
+			cv.new_graph()
+			cv.vert(x1 + 10,y1 - 10)	
+			cv.vert(x1 - 10,y1 + 10)	
+			cv.new_graph()
+			cv.vert(x1 - 10,y1 - 10)	
+			cv.vert(x1 + 10,y1 + 10)	
 
 class poly():
 	def __init__(self, s, a, p = None):
@@ -31,6 +61,11 @@ class picture():
 	def __init__(self):
 		self.q = poly(100, 45)
 		self.p = poly(100,12, self.q)
+		self.e1 = epi(100, 0, -0.1)
+		self.e2 = epi(100, 0,0.05, self.e1)
+	def draw_epi(self):
+		for t in range (300):
+			self.e2.draw(0, 0, t)
 	def draw(self, ox, oy):
 		self.p.draw(ox, oy)
 	def button_1(self, event):
@@ -40,6 +75,8 @@ cv = zvc.vancas(640, 640, 8, 8)
 cv.draw_grid()
 pic = picture()
 cv.bind(pic)
+pic.draw_epi()
 pic.draw(300, 200)
 pic.draw(100, 600)
+
 input("type anything to stop");
